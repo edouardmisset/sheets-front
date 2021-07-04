ffbe30f2cd2a42131c04a0eb5f108d0b919eb4f0
Edouard Misset <edouardmisset@gmail.com>
2021-07-04 22:50:27 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>Hi there! 🧗 📈</header>
+      <main></main>
+      <footer>Finally a footer that stays down! Awesome ✨</footer>
+    </>
+  );
+}
+
+export default App;
